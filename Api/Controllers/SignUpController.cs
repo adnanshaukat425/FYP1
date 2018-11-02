4abb7de0ba8675f163c1da42fef101efeb5dcc3d
Adnan <adnanshaukat425@gmail.com>
2018-11-02 13:18:30 +0500

    Changes in SignUpController

--- Api/Controllers/SignUpController.cs
+++ Api/Controllers/SignUpController.cs
@@ -9,32 +9,10 @@ namespace Api.Controllers
 {
     public class SignUpController : ApiController
     {
-        // GET: api/SignUp
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/SignUp/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/SignUp
         public void Post([FromBody]string value)
         {
             
-        }
-
-        // PUT: api/SignUp/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/SignUp/5
-        public void Delete(int id)
-        {
         }
     }
 }
